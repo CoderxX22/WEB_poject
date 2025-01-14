c90cd8f41d7311500557e2962a2be7a513acb6c3
DaniAyash <daniayash96@gmail.com>
2025-01-14 23:50:09 +0200

    Darkmode 90% fix (except login & refresh)

--- HealthVisualizer_next/src/app/components/Navbar.jsx
+++ HealthVisualizer_next/src/app/components/Navbar.jsx
@@ -4,21 +4,13 @@ import Link from 'next/link';
 
 const healthIcon = '/pngwing.com.png';
 
-const Navbar = ({links}) => {
+const Navbar = ({ links }) => {
     // State for the hamburger menu toggle
     const [menuOpen, setMenuOpen] = useState(false);
 
     // Function to toggle the hamburger menu visibility
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
-    };
-
-    // State to manage the dropdown visibility
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-
-    // Function to toggle dropdown visibility
-    const toggleDropdown = () => {
-        setDropdownOpen(!dropdownOpen);
     };
 
     return (
@@ -31,8 +23,10 @@ const Navbar = ({links}) => {
                         alt="logo"
                         className="w-8 h-8 filter dark:invert-0 invert" // Invert color for light mode
                     />
-                    <Link href="/" className="text-xl font-bold text-white dark:text-blue-400 lg:hover:text-black lg:dark:hover:text-gray-200 lg:transition-all lg:hover:scale-110">
-                        HealthVisualizer
+                    <Link href="/" passHref>
+                        <span className="text-xl font-bold text-white dark:text-blue-400 lg:hover:text-black lg:dark:hover:text-gray-200 lg:transition-all lg:hover:scale-110">
+                            HealthVisualizer
+                        </span>
                     </Link>
                 </div>
 
@@ -47,14 +41,15 @@ const Navbar = ({links}) => {
                 <ul className={`lg:flex md:flex items-center space-x-6 ${menuOpen ? 'hidden' : 'hidden'} lg:block md:block`}>
                     {links.map((link, index) => (
                         <li key={index}>
-                            <a
-                                href={link.href}
-                                className="relative text-gray-100 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-400 transition-transform duration-200 
-                                    before:absolute before:bottom-0 before:left-0 before:w-0 before:h-0.5 before:bg-blue-200 dark:before:bg-blue-400 
-                                    before:transition-all before:duration-300 hover:before:w-full hover:scale-105"
-                            >
-                                {link.name}
-                            </a>
+                            <Link href={link.href} passHref>
+                                <span
+                                    className="relative text-gray-100 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-400 transition-transform duration-200 
+                                        before:absolute before:bottom-0 before:left-0 before:w-0 before:h-0.5 before:bg-blue-200 dark:before:bg-blue-400 
+                                        before:transition-all before:duration-300 hover:before:w-full hover:scale-105"
+                                >
+                                    {link.name}
+                                </span>
+                            </Link>
                         </li>
                     ))}
                     <DarkMode />
@@ -66,14 +61,15 @@ const Navbar = ({links}) => {
                         <ul className="flex flex-col items-center space-y-4 py-4">
                             {links.map((link, index) => (
                                 <li key={index}>
-                                    <a
-                                        href={link.href}
-                                        className="relative text-gray-100 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-400 transition-transform duration-200 
-                                            before:absolute before:bottom-0 before:left-0 before:w-0 before:h-0.5 before:bg-blue-200 dark:before:bg-blue-400 
-                                            before:transition-all before:duration-300 hover:before:w-full hover:scale-105"
-                                    >
-                                        {link.name}
-                                    </a>
+                                    <Link href={link.href} passHref>
+                                        <span
+                                            className="relative text-gray-100 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-400 transition-transform duration-200 
+                                                before:absolute before:bottom-0 before:left-0 before:w-0 before:h-0.5 before:bg-blue-200 dark:before:bg-blue-400 
+                                                before:transition-all before:duration-300 hover:before:w-full hover:scale-105"
+                                        >
+                                            {link.name}
+                                        </span>
+                                    </Link>
                                 </li>
                             ))}
                             <DarkMode />
