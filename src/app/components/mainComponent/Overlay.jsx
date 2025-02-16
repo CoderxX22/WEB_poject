0bdfafbc1df5250fb718163563a9bebef91bd848
CoderxX22 <157704987+CoderxX22@users.noreply.github.com>
2025-02-16 14:12:10 +0200

    Update Overlay.jsx

--- src/app/components/mainComponent/Overlay.jsx
+++ src/app/components/mainComponent/Overlay.jsx
@@ -22,7 +22,7 @@ const OverlayBg = ({
 
             {/* Overlay Content Section */}
             <div className="absolute top-2/3 w-full text-center px-4 z-10">
-                <h1 className="text-3xl md:text-5xl lg:text-6xl font-extrabold mb-4 md:mb-6">
+                <h1 className="text-2xl md:text-5xl lg:text-6xl font-extrabold mb-4 md:mb-6">
                     <span className="text-gray-800 dark:text-gray-100">{spanText1}</span>{' '}
                     <span className="text-blue-600 dark:text-blue-400">{spanText2}</span>
                 </h1>
